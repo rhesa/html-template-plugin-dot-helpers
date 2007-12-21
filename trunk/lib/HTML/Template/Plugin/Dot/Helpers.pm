5479763d9de4f0523121887f8f87d5a5c162c337
Rhesa Rozendaal <rhesa@cpan.org>
2007-12-21 01:14:52 +0000

    new release. fixed a minor problem in the test suite
    git-svn-id: svn+ssh://cvs.mysitehosting.net/rhesa/HTML-Template-Plugin-Dot-Helpers@38 2c154bd8-bff5-0310-84c2-959dd6956e37

--- trunk/lib/HTML/Template/Plugin/Dot/Helpers.pm
+++ trunk/lib/HTML/Template/Plugin/Dot/Helpers.pm
@@ -17,11 +17,11 @@ HTML::Template::Plugin::Dot::Helpers - Add useful objects to your templates
 
 =head1 VERSION
 
-Version 0.02
+Version 0.03
 
 =cut
 
-our $VERSION = '0.02';
+our $VERSION = '0.03';
 
 =head1 SYNOPSIS
 
